54d9af54570732f94ebca30c01d19fca43d8da70
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-04-25 00:50:20 +0500

    add day4

---
+++ Day4_Python_MCQS/code.py
@@ -0,0 +1,119 @@
+# Q1
+for i in range(3):
+    print("Hello")
+
+# Q2
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+
+# Q3
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+# Q4
+for i in range(5):
+    if i == 2:
+        break
+    print(i)
+
+# Q5
+print(list(range(5)))
+
+# Q6
+fruits = ["apple", "banana"]
+for fruit in fruits:
+    print(fruit)
+
+# Q7
+for i in range(5):
+    if i == 2:
+        pass
+    print(i)
+
+# Q8
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+
+# Q9
+# while True:
+#     print("Hi")  # Infinite loop (commented to prevent running forever)
+
+# Q10
+for i in range(1, 101):
+    print(i)
+
+# Q11
+print(list(range(3)))
+
+# Q12
+print(list(range(1, 6)))
+
+# Q13
+print(list(range(0, 10, 2)))
+
+# Q14
+for el in [1, 4, 9, 16]:
+    print(el)
+
+# Q15
+for i in range(10):
+    if i == 3:
+        break
+
+# Q16
+# while True:
+#     print("Hi")  # Infinite loop (commented)
+
+# Q17
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+# Q18
+print(list(range(10, 0, -1)))
+
+# Q19
+for i in range(5):
+    print(i)
+
+# Q20
+for i in range(3):
+    print(i)
+else:
+    print("Done")
+
+# Q21
+condition = False
+while condition:
+    print("Running...")
+
+# Q22
+for el in range(5):
+    pass  # Does nothing
+
+# Q23
+mylist = [1, 2, 3]
+for el in mylist:
+    print(el)
+
+# Q24
+for i in range(2):
+    print(i)
+while i < 2:
+    print(i)
+    i += 1
+
+# Q25
+for i in range(5):
+    if i == 1:
+        continue
+    if i == 3:
+        break
+    print(i)
